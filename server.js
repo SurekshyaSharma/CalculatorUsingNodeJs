325c7605a54313fcfdaf027a0390fc48001a6be3
SurekshyaSharma <shar3415@stthomas.edu>
2021-03-02 17:57:56 -0600

    Added new files

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 const express = require('express');
 const XLSX = require("xlsx");
-
 const http = require('http');
+
 var bodyParser = require('body-parser')
 
 //reading excel file
 const workbook = XLSX.readFile('userLog.xlsx');
 const sheet_name_list = workbook.SheetNames;
 console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]));
-const messages = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-
+const messages = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
 const app = express();
 const port = 8000;
-
 //connecting the html with the express server
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+//write in excel
+
+
+
 //make an request to read and show in backend => front end
 app.get(`/messages`, (req, res) => {
-    res.send( XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
-})
-//make an request to frontend => back end and 
-app.post(`/messages`, (req, res) => {
-    res.send( XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+    res.send( messages)
 })
 
+// make an request to frontend => back end and 
+app.post(`/newItem`, (req, res) => {
+    res.send( `post request /newItem route on ${PORT}`)
+})
+
+app.put(`/item`, (req, res) => {
+  res.send( `post request /newItem route on ${PORT}`)
+})
 app.listen(port, () => {
   console.log(`Node server listening on port ${port}!`)
   console.log(`go to http://localhost:8000/`)
